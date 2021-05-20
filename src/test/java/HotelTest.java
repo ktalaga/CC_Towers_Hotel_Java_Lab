e333f2a699d0c260d24974351d6163658520c587
Andrew Sharkey <sharkand@hotmail.com>
2021-05-20 14:33:33 +0100

    First Hotel test passed.

---
+++ src/test/java/HotelTest.java
@@ -0,0 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class HotelTest {
+
+    private Hotel hotel;
+    private Bedroom bedroom_1;
+    private Bedroom bedroom_2;
+    private Bedroom bedroom_3;
+    private ConferenceRoom conferenceRoom_1;
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+    @Before
+    public void before() {
+        bedroom_1 = new Bedroom(1, RoomType.SINGLE);
+        bedroom_2 = new Bedroom(2, RoomType.DOUBLE);
+        bedroom_3 = new Bedroom(3, RoomType.FAMILY);
+        bedrooms = new ArrayList<>();
+        bedrooms.add(bedroom_1);
+        bedrooms.add(bedroom_2);
+        bedrooms.add(bedroom_3);
+        conferenceRoom_1 = new ConferenceRoom("Glendevon", 100, true);
+        conferenceRooms = new ArrayList<>();
+        conferenceRooms.add(conferenceRoom_1);
+        hotel = new Hotel(bedrooms, conferenceRooms);
+    }
+
+    @Test
+    public void shouldHaveBedrooms() {
+        assertEquals(3, hotel.getNumberOfBedrooms());
+    }
+
+
+}
