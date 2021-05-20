38b76b9b92fa3107a2ded18051ef71796393c258
konrad <ktalaga90@gmail.com>
2021-05-20 11:53:56 +0100

    Room class amended

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -2,16 +2,10 @@ import java.util.ArrayList;
 
 public abstract class Room {
 
-    private String capacity;
     private ArrayList<Guest> guestsList;
 
-    public Room(String capacity, ArrayList<Guest> guestsList) {
-        this.capacity = capacity;
+    public Room(ArrayList<Guest> guestsList) {
         this.guestsList = new ArrayList<Guest>();
-    }
-
-    public String getCapacity() {
-        return capacity;
     }
 
     public ArrayList<Guest> getGuestsList() {
